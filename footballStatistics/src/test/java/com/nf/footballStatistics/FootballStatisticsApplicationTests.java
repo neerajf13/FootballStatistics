4fbbaf2ba5f038f7c3bed2bfe6a373efbd767882
ferna <ferna@DESKTOP-0IONUK8.fios-router.home>
2020-05-22 16:40:23 -0400

    Initial project set up

---
+++ footballStatistics/src/test/java/com/nf/footballStatistics/FootballStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nf.footballStatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
